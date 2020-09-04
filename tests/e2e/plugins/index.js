e59df7e746476b32a28f09b44a51fff091c60207
munlite-team <68898888+munlite-team@users.noreply.github.com>
2020-09-04 16:48:26 +0700

    Circleci project setup (#2)
    * Activate Vercel
    
    * Add .circleci/config.yml
    
    * Add automated testing with cypress
    
    * Fix linting issue & Upgrade dependencies
    
    * Change workflow to lint and test
    
    * Add npx to command
    
    * Change serve command
    
    * Add wait-on
    
    * add npx command for wait-on
    
    * Change wait to new line
    
    * Remove config
    
    Co-authored-by: Kevin Nathaniel <kevinnathaniel497@yahoo.com>

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -8,11 +8,11 @@
 
 // /* eslint-disable import/no-extraneous-dependencies, global-require */
 // const webpack = require('@cypress/webpack-preprocessor')
-const cucumber = require('cypress-cucumber-preprocessor').default
+const cucumber = require('cypress-cucumber-preprocessor').default;
 
-module.exports = (on, config) => {
-  on('file:preprocessor', cucumber())
-}
+module.exports = (on) => {
+  on('file:preprocessor', cucumber());
+};
 
 module.exports = (on, config) => {
   // on('file:preprocessor', webpack({
