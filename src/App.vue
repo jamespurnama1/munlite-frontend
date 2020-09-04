d107e2146e94a8346b431cd9148f65216c7b9fe8
samanthatandrii <samantha.tandri@gmail.com>
2020-09-04 10:24:26 +0700

    finish navbar styling

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <div class="navLogo">
-        <img src="@/assets/img/logo_main.png" />
-      </div>
+    <div class="nav">
       <div class="navLeft">
-        <router-link to="/">Overview</router-link>
-        <router-link to="/delegates">Delegates</router-link>
-        <router-link to="/motions">Motions</router-link>
-        <router-link to="/caucus">Caucus</router-link>
-        <router-link to="/crisis">Crisis</router-link>
+        <div class="navLogo">
+          <img id="logo" src="@/assets/img/logo_main.png" />
+        </div>
+        <div class="navTab">
+          <router-link to="/" @click.native="onTabClick">Overview</router-link>
+          <router-link to="/delegates" @click.native="onTabClick">Delegates</router-link>
+          <router-link to="/motions" @click.native="onTabClick">Motions</router-link>
+          <router-link to="/caucus" @click.native="onTabClick">Caucus</router-link>
+          <router-link to="/crisis" @click.native="onTabClick">Crisis</router-link>
+          <div
+            class="border"
+            :style="{
+              left: `${borderStyles.left-1}px`,
+              width: `${borderStyles.width}px`,
+              height: `${borderStyles.height}px`
+            }"
+          ></div>
+        </div>
       </div>
       <div class="navRight">
         <a>Settings</a>
@@ -20,6 +30,51 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'MUN',
+  data() {
+    return {
+      border: {
+        left: 0,
+        width: 0,
+        height: 0,
+      },
+      borderTemp: null,
+    };
+  },
+  computed: {
+    borderStyles() {
+      return this.borderTemp == null ? this.border : this.borderTemp;
+    },
+  },
+  mounted() {
+    const tab = document.getElementsByClassName('router-link-exact-active');
+    if (tab[0]) {
+      const styles = {
+        left: tab[0].offsetLeft,
+        width: tab[0].clientWidth,
+        height: tab[0].clientHeight,
+      };
+      console.log(tab[0].clientHeight);
+      this.border = styles;
+    }
+  },
+  methods: {
+    onTabClick(evt) {
+      console.log('clicked');
+      const tab = evt.target;
+      const styles = {
+        left: tab.offsetLeft,
+        width: tab.clientWidth,
+        height: tab.clientHeight,
+      };
+      this.border = styles;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @import '@/styles/index.scss';
