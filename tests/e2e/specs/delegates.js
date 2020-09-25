1973cfd5ad4c6470495cc15c97e286f78c35fdaf
samanthatandrii <samantha.tandri@gmail.com>
2020-09-25 22:04:32 +0700

    fix testing delegates page

--- tests/e2e/specs/delegates.js
+++ tests/e2e/specs/delegates.js
@@ -4,13 +4,19 @@ describe('Landing Page Test', () => {
 
     cy.contains('h1', 'Delegates');
     cy.contains('button', 'Role Call');
+    cy.get('div[class="button"]').click();
+    cy.get('input').should('exist');
 
     // check modal if button clicked
 
-    cy.get('table[class="table"]>tr[class="data"]').each(() => {
-      cy.get('td[class="presence"]').should('exist');
-      cy.get('td[class="name"]').should('exist');
+    cy.get('div[class="table-data"]>div[class="data"]').each(() => {
+      cy.get('p[class="presence"]').should('exist');
+      cy.get('p[class="name"]').should('exist');
       cy.get('img[class="img"]').should('exist');
+    });
+
+    cy.get('div[class="info"]>div[class="info-data"]').each(() => {
+      cy.get('p[class="data"]').should('exist');
     });
   });
 });
