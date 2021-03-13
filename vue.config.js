7c5a9dabda76a4e1a2ccf4bbe3ddceb800fbec5d
jamespurnama1 <jamespurnama1@users.noreply.github.com>
2021-03-14 03:13:47 +0700

    Updated favicon PWA configuration

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,41 @@ module.exports = {
       },
     },
   },
+  pwa: {
+    name: 'MUNlite',
+    themeColor: '#5f78ff',
+    msTileColor: '#5f78ff',
+    appleMobileWebAppCapable: 'yes',
+    assetsVersion: '',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-144x144.png',
+    },
+    manifestOptions: {
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+      display: 'standalone',
+      start_url: '/',
+    },
+    workboxOptions: {
+      cacheId: 'draft',
+      importWorkboxFrom: 'local',
+      navigateFallback: 'shell.html',
+      navigateFallbackWhitelist: [/^((?!\/404).)*$/],
+    },
+  },
 };
